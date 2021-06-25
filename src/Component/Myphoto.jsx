72c3b282440874e91558acd8afc2af89e26c1b9c
aditya kumar <adityakumar1995rawat@gmail.com>
2021-06-25 16:41:46 +0530

    for project

---
+++ src/Component/Myphoto.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export default class Myphoto extends Component {
+    static propTypes = {
+        prop: PropTypes
+    }
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+    }
+    
+
+    render() {
+        return (
+            <div>
+            
+            </div>
+        )
+    }
+}
---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Navbar.css';
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav className="navbar">
+                <div className="title">
+                    Search Photos
+                    <div className="search-box">
+                        <input type="search" name="search" id="search" />
+                    </div>
+                
+                </div>
+            </nav>
+        </div>
+    )
+}
